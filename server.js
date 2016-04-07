425772af0382509e6e4338ebf8b5e5a045d76549
JellyKid <seaoftea@gmail.com>
2016-04-07 15:30:51 -0400

    Adding shorturl module

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ var app = express();
 //place new modules here
 var parsetime = require(path.resolve(modPath,'timestamp'));
 var whoami = require(path.resolve(modPath,'whoami'));
+var shorturl = require(path.resolve(modPath,'shorturl'));
 
 app.get('/',function(req,res){
   res.send('hello world');
 });
 app.use('/time',parsetime);
 app.get('/whoami', whoami);
+app.use('/short',shorturl);
+
 
 app.listen(port, function(){
   console.log('Express server started on port ' + port);
