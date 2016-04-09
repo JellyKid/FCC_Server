fd36dd989f5d84ed693d0b1d65739ea59936b587
JellyKid <seaoftea@gmail.com>
2016-04-09 17:58:03 -0400

    Finished project ready to go up to api server

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var app = express();
 var parsetime = require(path.resolve(modPath,'timestamp'));
 var whoami = require(path.resolve(modPath,'whoami'));
 var shorturl = require(path.resolve(modPath,'shorturl'));
+var imgsearch = require(path.resolve(modPath,'imgsearch'));
 
 app.get('/',function(req,res){
   res.send('hello world');
@@ -16,7 +17,8 @@ app.get('/',function(req,res){
 app.use('/time',parsetime);
 app.get('/whoami', whoami);
 app.use('/short',shorturl);
-
+app.get('/imgsearch/:search',imgsearch.getClientId,imgsearch.buildPath, imgsearch.newRequest, imgsearch.sendData,imgsearch.logSearch);
+app.get('/latest/imgsearch', imgsearch.getLog);
 
 app.listen(port, function(){
   console.log('Express server started on port ' + port);
